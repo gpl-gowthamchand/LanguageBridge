2093ca7cc708298df058de0882c69be72873fbe8
Gowthamchand5102 <gouthamchand5102@gmail.com>
2025-04-08 15:08:37 +0530

    removed unwanted tags

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -18,6 +17,11 @@ export default defineConfig(({ mode }) => ({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ['dotenv'], // Added dotenv to external
     },
   },
 }));
